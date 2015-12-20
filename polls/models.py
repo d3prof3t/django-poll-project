11521065a431407f52715470d346767b0e371320
Saurabh Sharma <saurabhpysharma@gmail.com>
2015-12-20 23:06:46 +0530

    v1 for web based poll application

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+# polls/models.py
+
+import datetime
+
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.TextField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
