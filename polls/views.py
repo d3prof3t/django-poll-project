c9a1245eeb9e40d7255c050fb181e4c1a26ba3af
Saurabh Sharma <saurabhpysharma@gmail.com>
2015-12-21 00:24:48 +0530

    generic views

--- polls/views.py
+++ polls/views.py
@@ -3,27 +3,52 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
-from .models import Question
-from .models import Choice
+from .models import Question, Choice
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/results.html', context)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {'question': question}
+#     return render(request, 'polls/detail.html', context)
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {'question': question}
+#     return render(request, 'polls/results.html', context)
+
+
+# Class based generic views
+class IndexView(generic.ListView):
+
+    """docstring for IndexView"""
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """return the last 5 published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+
+    """docstring for DetailView"""
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+
+    """docstring for DetailView"""
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
